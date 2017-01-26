603a85b001d994e3285b399baff034295cef638a
Sridhar Vadlamani <v.sridhar.sreenivas@gmail.com>
2017-01-26 15:00:10 -0800

    from Starter Template

---
+++ doodle/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ENVIRONMENT['hosts']
+
+INSTALLED_APPS += [
+
+]
+
+if SITE_PREFIX:
+    STATIC_URL = '/{}/static'.format(SITE_PREFIX)
+    MEDIA_URL = '/{}/media'.format(SITE_PREFIX)
+    SESSION_COOKIE_PATH = '/{}'.format(SITE_PREFIX)
+    LOGIN_REDIRECT_URL = '/{}/'.format(SITE_PREFIX)
+    LOGIN_URL = '/{}/accounts/login/'.format(SITE_PREFIX)
+    LOGOUT_URL = '/{}}/accounts/logout/'.format(SITE_PREFIX)
