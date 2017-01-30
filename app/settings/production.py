145d8063ddabd3b558ecb32a3b82d57591cffe9f
Sridhar Vadlamani <doodla@doodla.me>
2017-01-30 09:58:52 +0000

    Fixed Login Issues

--- app/settings/production.py
+++ app/settings/production.py
@@ -11,7 +11,8 @@ INSTALLED_APPS += [
 if SITE_PREFIX:
     STATIC_URL = '/{}/static/'.format(SITE_PREFIX)
     MEDIA_URL = '/{}/media/'.format(SITE_PREFIX)
-    SESSION_COOKIE_PATH = '/{}'.format(SITE_PREFIX)
+    SESSION_COOKIE_PATH = '/{}/'.format(SITE_PREFIX)
     LOGIN_REDIRECT_URL = '/{}/'.format(SITE_PREFIX)
     LOGIN_URL = '/{}/accounts/login/'.format(SITE_PREFIX)
     LOGOUT_URL = '/{}/accounts/logout/'.format(SITE_PREFIX)
+    SESSION_COOKIE_NAME = '{}sessionid'.format(SITE_PREFIX)
