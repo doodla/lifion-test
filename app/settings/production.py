f20356d9016bcf26cf9fd1900b0f2b81bb562911
Sridhar Vadlamani <v.sridhar.sreenivas@gmail.com>
2017-01-30 01:27:57 -0800

    Media URL must end with a slash

--- app/settings/production.py
+++ app/settings/production.py
@@ -10,7 +10,7 @@ INSTALLED_APPS += [
 
 if SITE_PREFIX:
     STATIC_URL = '/{}/static'.format(SITE_PREFIX)
-    MEDIA_URL = '/{}/media'.format(SITE_PREFIX)
+    MEDIA_URL = '/{}/media/'.format(SITE_PREFIX)
     SESSION_COOKIE_PATH = '/{}'.format(SITE_PREFIX)
     LOGIN_REDIRECT_URL = '/{}/'.format(SITE_PREFIX)
     LOGIN_URL = '/{}/accounts/login/'.format(SITE_PREFIX)
